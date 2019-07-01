931dd02908f140f73c65f15d4401fb41de5014d8
adalar ugur <adalarugur@gmail.com>
2019-07-01 17:32:39 +0300

    bodyparser entegre edildi

--- serverside/main.js
+++ serverside/main.js
@@ -1,7 +1,8 @@
 var express = require('express')
 var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
 
-var app = express();
+var app = express(bodyParser.json);
 
 mongoose.connect('mongodb+srv://adalar:ugur321ada@cluster0-0hwtx.mongodb.net/test?retryWrites=true&w=majority',
 err=>{
